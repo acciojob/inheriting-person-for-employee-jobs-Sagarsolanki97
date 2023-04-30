a5afd9e40fecb6cf4239b41bf2a73a8b10e60b69
SAGAR SOLANKI <118007645+Sagarsolanki97@users.noreply.github.com>
2023-04-30 11:17:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function Person(name, age) {
   this.age = age;
 }
 
-// Adding greet method to Person prototype
+// Add greet method to Person prototype
 Person.prototype.greet = function() {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-};
+}
 
 // Employee constructor function
 function Employee(name, age, jobTitle) {
@@ -15,13 +15,11 @@ function Employee(name, age, jobTitle) {
   this.jobTitle = jobTitle;
 }
 
-// Inheriting Person prototype in Employee prototype
+// Inherit Person prototype in Employee prototype
 Employee.prototype = Object.create(Person.prototype);
-
-// Fixing the constructor property on Employee prototype
 Employee.prototype.constructor = Employee;
 
-// Adding jobGreet method to Employee prototype
+// Add jobGreet method to Employee prototype
 Employee.prototype.jobGreet = function() {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-};
+}
